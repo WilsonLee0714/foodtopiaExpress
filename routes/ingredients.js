8e38949e09c5e45cee3dc6eb6ee5ee5ae1f35a19
brian <penguin410925@gmail.com>
2018-12-04 09:31:59 +0800

    "1204 revise ingredients.js"

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -15,18 +15,83 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
-// http://localhost:3000/api/ingredients
+// http://localhost:3000/api/ingredients_meat
 router
-  .route("/ingredients")
+  .route("/ingredients_meat")
   .get(function(req, res) {
     //GET http://localhost:3000/xxx/ingredients
     //  res.send("get all product");
-    connection.query("select * from igr_test", function(error, results) {
+    connection.query("select * from igr_test where product_id between 1101 and 1517 ", function(error, results) {
       if (error) throw error;
       res.json(results);
     });
   })
 
- 
+// http://localhost:3000/api/ingredients_dairy
+  router
+  .route("/ingredients_dairy")
+  .get(function(req, res) {
+    //GET http://localhost:3000/xxx/ingredients
+    //  res.send("get all product");
+    connection.query("select * from igr_test where product_id between 2101 and 2501 ", function(error, results) {
+      if (error) throw error;
+      res.json(results);
+    });
+  })
+
+
+// http://localhost:3000/api/ingredients_other
+router
+.route("/ingredients_other")
+.get(function(req, res) {
+  //GET http://localhost:3000/xxx/ingredients
+  //  res.send("get all product");
+  connection.query("select * from igr_test where product_id between 3101 and 3703 ", function(error, results) {
+    if (error) throw error;
+    res.json(results);
+  });
+})
+
+// http://localhost:3000/api/ingredients_seafood
+router
+.route("/ingredients_seafood")
+.get(function(req, res) {
+  //GET http://localhost:3000/xxx/ingredients
+  //  res.send("get all product");
+  connection.query("select * from igr_test where product_id between 4101 and 4246 ", function(error, results) {
+    if (error) throw error;
+    res.json(results);
+  });
+})
+
+// http://localhost:3000/api/ingredients_fruit
+router
+.route("/ingredients_fruit")
+.get(function(req, res) {
+  //GET http://localhost:3000/xxx/ingredients
+  //  res.send("get all product");
+  connection.query("select * from igr_test where product_id between 5101 and 5131 ", function(error, results) {
+    if (error) throw error;
+    res.json(results);
+  });
+})
+
+// http://localhost:3000/api/ingredients_vegetable
+router
+.route("/ingredients_vegetable")
+.get(function(req, res) {
+  //GET http://localhost:3000/xxx/ingredients
+  //  res.send("get all product");
+  connection.query("select * from igr_test where product_id between 6101 and 6827 ", function(error, results) {
+    if (error) throw error;
+    res.json(results);
+  });
+})
+
+
+  // SELECT * FROM igr_test
+  // WHERE product_name
+  // BETWEEN 101001 AND 101020;
+
 
 module.exports = router;
