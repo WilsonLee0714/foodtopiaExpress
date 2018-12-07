339c400899aee0d0e7ae0ac7f7b04237baf9d4b2
yvonne <yvonne250003@gmail.com>
2018-12-07 19:59:37 +0800

    1207 subcate filter

--- routes/recipe_list.js
+++ routes/recipe_list.js
@@ -18,7 +18,7 @@ router.route('/recipe_list')
     .get(function(req, res){
         //GET http://localhost:3000/xxx/recipe
         // res.send("get all recipe")
-        connection.query('select * from `menu` WHERE `difficult` Like "困難" limit 3', function(error, results) {
+        connection.query('select * from `sector` ', function(error, results) {
             if(error) throw error;
             res.json(results)
             console.log("Database foodtopia connected")
@@ -30,27 +30,40 @@ router.route('/recipe_list')
     // })
 
 // http://localhost:3000/api/recipe/1
-router.route('/recipe_list/1')
+router.route('/recipe_list/:id')
 .get(function(req, res){
-    //GET http://localhost:3000/api/recipe/2  
+    //GET http://localhost:3000/api/recipe_list/2  
     // 讀取:id參數的值 req.params.id
     // res.send("get recipe id " + req.params.id);
     connection.query(
-        'SELECT * FROM `menu`', function(error,results){
-            console.log(results)
+        'SELECT * FROM `sector` WHERE `category_id`=?', req.params.id,function(error,results){
+            // console.log(results)
         if(error) throw error;
         res.json(results)
         
       })
 
 })
-// .put(function(req, res){
-//     //GET http://localhost:3000/api/recipe/2 
-//     res.send("修改" + req.params.id + "資料");
-// })
-// .delete(function(req, res){
-//     //GET http://localhost:3000/api/recipe/2  
-//     res.send("刪除" + req.params.id + "資料");
-// })
+
+
+// 子分類下的食譜
+router.route('/recipe_sub/1')
+.get(function(req, res){
+    //GET http://localhost:3000/api/recipe_list/sub2  
+    // 讀取:id參數的值 req.params.id
+    // res.send("get recipe id " + req.params.id);
+    // var subCateId = [ 'country_id', 'serving_id', 'occasion_id', 'difficult_id', 'time_id' ] 
+    connection.query(
+        'SELECT * FROM `menu` WHERE `category_id`=1', function(error,results){
+            // console.log(results)
+        if(error) throw error;
+        res.json(results);
+        // console.log(req.params.id)
+      })
+})
+// how do i read different column when i have variable category id?
+//  xxxFAILxxx 1. we can't write variables into SQL query  
+//  2. we try to write different fetch API? how many api would it be?
+
 
 module.exports = router;
