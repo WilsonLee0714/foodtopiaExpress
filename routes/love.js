f7c40a009a596353d550505e61f0c32d068bce12
John Doe <johndoe@example.com>
2018-12-10 09:19:20 +0800

    ming/12.10/09:20/收藏頁面完成

--- routes/love.js
+++ routes/love.js
@@ -45,5 +45,37 @@ router
       }
     })
   })
+  //收藏依照會員搜尋
+  router
+  .route("/love_m")
+  .get(function(req, res) {
+    connection.query("SELECT `menu`.*, `love`.`recipe_id` FROM `love` JOIN `menu` ON `menu`.`id`=`love`.`recipe_id` where `love`.`member_id`=?",[req.session.sid],function(error,rows){
+      if (error) throw error;
+      res.json(rows);
+    })
+  })
+
+  // .route("/love")
+  // .post(function(req, res) {
+  //   var _body = req.body;
+  //   connection.query("SELECT * FROM `love` where `member_id`=? AND `recipe_id`=?",[req.session.sid,_body.recipe_id],function(error,rows){
+  //     if(!rows.length){
+  //       connection.query("INSERT INTO love set ?",{member_id:req.session.sid,recipe_id:_body.recipe_id},function(error){
+  //         if (error) throw error;
+  //         res.json({ message: "新增成功" });
+  //      })
+  //     }else if(res.rows==1){
+  //      connection.query("INSERT INTO love set ?",{member_id:req.session.sid,recipe_id:_body.recipe_id,love=2},function(error){
+  //         if (error) throw error;
+  //         res.json({ message: "取消收藏" });
+  //      })
+  //     }else {
+  //      connection.query("INSERT INTO love set ?",{member_id:req.session.sid,recipe_id:_body.recipe_id,love=1},function(error){
+  //         if (error) throw error;
+  //         res.json({ message: "加入收藏" });
+  //      })
+  //      }
+  //   })
+  // })
 
 module.exports = router;
